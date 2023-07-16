53ce2514ad073f9a7237d57dedc0b2f804dfc1f5
Idris <fortunejeremy14@gmail.com>
2023-07-15 23:24:43 -0700

    added a main description

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+
+/**
+ * main - prints with proper grammar, but the outcome is a piece of art,
+ *        followed by a new line
+ * Return:0
+ *
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
