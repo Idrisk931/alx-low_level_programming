13e340f99be84839ce190d977d7d26f63de0b2c6
Idris <fortunejeremy14@gmail.com>
2023-07-21 10:27:25 -0700

    Debbuging

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -5,13 +5,13 @@
 * Return: 0
 */
 
-int main(void)
-
+int main(void
+		)
 {
-			int i;
+	int i;
 
-			i = 98;
-			positive_or_negative(i);
+	i = 98;
+	positive_or_negative(i);
 
-		return (0);
+	return (0);
 }
